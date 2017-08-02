50d723e07a1d9568030ada19a743ffa4135e95f3
Jason Springer <jsnsprngr@gmail.com>
2017-08-02 00:16:04 -0400

    Adjust slideUpOnScrollDown() settings.

--- view/js/script.js
+++ view/js/script.js
@@ -263,7 +263,7 @@ function slideUpOnScrollDown() {
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            bottom_of_window = bottom_of_window + 200;  
+            bottom_of_window = bottom_of_window + 250;  
           
             if( bottom_of_window > bottom_of_object ){
                 
